da3db5407eaf2f8790860781be95ddf5415444c6
mrgruby <mrgruby@gmail.com>
2023-02-26 13:03:25 +0100

    Added controller

---
+++ AtpCase/Controllers/AllocationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServerAllocator;
+
+namespace AtpCase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AllocationController : ControllerBase
+    {
+        [HttpGet("AllocateServer")]
+        public IActionResult Allocate(string serverType) 
+        {
+            var res = AllocateServer.Allocate(serverType);
+            return Ok(res);
+        }
+    }
+}
